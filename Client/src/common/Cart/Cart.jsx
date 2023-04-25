cd7c0ce30f4677d2599591fcb2e1d2033e4b60ab
DilshanLakindu <dilshanlakindu5@gmail.com>
2023-04-26 01:06:40 +0530

    Cart

--- Client/src/common/Cart/Cart.jsx
+++ Client/src/common/Cart/Cart.jsx
@@ -27,7 +27,7 @@ const Cart = ({ CartItem, addToCart, decreaseQty }) => {
                   <div className='cart-details'>
                     <h3>{item.name}</h3>
                     <h4>
-                      ${item.price}.00 * {item.qty}
+                      rs{item.price}.00 * {item.qty}
                       <span>${productQty}.00</span>
                     </h4>
                   </div>
